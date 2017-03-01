e4103567830adf500f19c00edc75287a66543227
Hitesh Khandelwal <hitesh@smartprix.com>
2017-03-01 15:01:41 +0530

    add fieldset file

---
+++ src/Fieldset.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="ela-fieldset">
+		<div class="ela-fieldset-legend" v-if="legend" v-text="legend"></div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ela-fieldset',
+	props: {
+		legend: String,
+	},
+};
+</script>
+
+<style>
+.ela-fieldset {
+	padding: 16px 16px 0 16px;
+	border: #bfcbd9 1px solid;
+	margin: 15px 0 22px 0;
+	background: ghostwhite;
+	position: relative;
+	border-radius: 5px;
+
+	> .ela-fieldset-legend {
+		padding: 5px 15px 5px 15px;
+		background: #bfcbd9;
+		color: darkslategray;
+		font-weight: bold;
+		font-size: 13px;
+		position: absolute;
+		top: -15px;
+		left: -9px;
+		padding: 2px 9px;
+		border-radius: 3px;
+	}
+
+	.el-row:last-child > .el-col {
+		margin-bottom: -6px;
+	}
+
+	> .el-form-item:last-child {
+		margin-bottom: 16px;
+	}
+}
+</style>
